42b9e09068931316767e4785cced1b35de4e7cb3
Johan Vermaak <10015918@student.toiohomai.ac.nz>
2018-09-10 11:39:43 +1200

    incomplete assignments

---
+++ Array/Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[10];
+            Console.WriteLine("<--Arrays-->/nPlease enter 10 numbers.");
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Number {i+1}: ");
+                int num = int.Parse(Console.ReadLine());
+                array1[i] = num;
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Number {i + 1}: {array1[i]}");
+            }
+        }
+    }
+}
---
+++ Array/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 1000;
+            Random rand = new Random();
+            Console.WriteLine("Random numbers being generated...");
+            int[] array1 = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array1[i] = rand.Next(1000, 9999);
+            }
+
+            int check;
+            
+
+            for (int j = 0; j < array1.Length; j++)
+            {
+                int total = 0;
+                check = array1[j];
+                for(int k = 0;k < array1.Length; k++)
+                {
+                    if (array1[k] == check)
+                        total += 1;
+                }
+                Console.WriteLine($"array1 index {j+1} occured {total} times.");
+            }
+        }
+    }
+}
